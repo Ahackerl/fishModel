81550c6d8301cf3a2d8983b4c3be649d56dbac52
Administrator <941131649@qq.com>
2021-04-29 21:27:28 +0800

    修复guolin permissionX

--- permission/src/main/java/com/permissionx/guolindev/PermissonXOnJava.kt
+++ permission/src/main/java/com/permissionx/guolindev/PermissonXOnJava.kt
@@ -1,15 +1,14 @@
 package com.permissionx.guolindev
 
-import android.Manifest
 import androidx.fragment.app.FragmentActivity
 
 abstract class PermissonXOnJava {
 
     abstract fun callBack(allGranted: Boolean, deniedList: List<String>)
 
-    fun request(activity: FragmentActivity,permissionStr:String){
+    fun request(activity: FragmentActivity,vararg permissionStr:String){
 
-        PermissionX.request(activity, permissionStr) { allGranted, deniedList ->
+        PermissionX.request(activity, *permissionStr) { allGranted, deniedList ->
             callBack(allGranted,deniedList)
         }
     }
